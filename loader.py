38fc15544285e95e654c56efc9c1caa98892bb9b
Manuel Pena <mpena991@gmail.com>
2025-05-16 12:27:33 +0200

    added argparser and ranges

--- loader.py
+++ loader.py
@@ -17,13 +17,13 @@ class FresnelFile(Enum):
     TWO_DIEL4 = "twodielTM_4f.txt"
 
 
-freqs = {FresnelFile.TWO_DIEL4 : 4, 
+frequencies = {FresnelFile.TWO_DIEL4 : np.linspace(4,16,4), 
          FresnelFile.TWO_DIEL8 : np.linspace(1,8,8),
          FresnelFile.U         : np.linspace(2,16,8),
-         FresnelFile.ONE_DIEL4 : 4,
-         FresnelFile.ONE_DIEL8 : 8,
-         FresnelFile.RECT_OFF  : 8,
-         FresnelFile.RECT_CENT : 4}
+         FresnelFile.ONE_DIEL4 : np.linspace(4,16,4),
+         FresnelFile.ONE_DIEL8 : np.linspace(1,8,8),
+         FresnelFile.RECT_OFF  : np.linspace(2,16,8),
+         FresnelFile.RECT_CENT : np.linspace(4,16,4)}
 
 
 
@@ -32,36 +32,61 @@ N_M = 49
 N_R = 72
 
 
-def load_file( filename : FresnelFile, folder = Path('./Data') ):
+def load_file( filename : FresnelFile | str, folder = Path('./Data'), 
+              positive_exponent = True, 
+              zero_completed = True,
+              verbose = False):
+    """
+    """
+
+    if isinstance(filename,str):
+        filename = FresnelFile(filename)
+
+    folder = Path(folder)
+
     file_route = folder / filename.value
+    
+    f = frequencies[filename]*1E9
 
-# with open(file_route) as f:
-#         for n in range(5):
-#             line = f.readline()
-#    freq_info = line.split(" : ")[1].split(" ")
-#     N_F = int(freq_info[0])
-#    print(freq_info)
-    print(freqs[filename])
+    N_F = len(f)
 
-
-    N_F = len(freqs[filename])
-
-    print(f"loading file {filename} located at {filename.value}, with {N_F} frecuencies ... ")
+    if verbose:
+        print(f"loading file {filename} located at {filename.value},\n with frecuencies:\n")
+        print(", ".join([f'{v:.1f} GHz' for v in frequencies[filename]] ))
     
     data = np.loadtxt(fname=file_route,skiprows=10)
     shape = (N_F, N_M, N_E)
     U_inc = ( data[:,3] - 1j*data[:,4]).reshape(shape,order="F")
     U_tot = ( data[:,5] - 1j*data[:,6]).reshape(shape,order="F")
+    
+    if positive_exponent:
+        U_inc = np.conjugate(U_inc)
+        U_tot = np.conjugate(U_tot)
+
     A_near = U_tot - U_inc
     r_ID = data[:,1].astype(int).reshape(shape,order="F") - 1
     A = np.zeros((N_F,N_R, N_E), dtype=np.complex128)
     U = np.zeros((N_F,N_R, N_E), dtype=np.complex128)
-    for f in range(N_F):
-        for e in range(N_E):
-            A[f,r_ID[f,:,e],e] = A_near[f,:,e]
-            U[f,r_ID[f,:,e],e] = U_inc[f,:,e]
+   
+    if zero_completed:
+        for f in range(N_F):
+            for e in range(N_E):
+                A[f,r_ID[f,:,e],e] = A_near[f,:,e]
+                U[f,r_ID[f,:,e],e] = U_inc[f,:,e]
 
-    return A, U
+    return A, U, f
 
 if __name__ == "__main__":
-    load_file(FresnelFile.U)
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--verbose', action="store_true")
+    parser.add_argument(
+        "--file",
+        type=str,
+        choices=[f.value for f in FresnelFile],  # shows up in help
+        help=f"Choose a file from: {[f.value for f in FresnelFile]}")
+
+
+    args = parser.parse_args()
+    
+    A, U, f =  load_file(args.file, verbose=args.verbose)
