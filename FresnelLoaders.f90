edf96fd51f6e136ecfebf257a478576f25883fa3
Manuel Pena <mpena991@gmail.com>
2025-05-19 16:44:20 +0200

    loading measured data done, needs zero completion

--- FresnelLoaders.f90
+++ FresnelLoaders.f90
@@ -1,17 +1,48 @@
 module FresnelLoaders
 
+        implicit none
+
         integer, parameter :: N_E = 36
         integer, parameter :: N_R = 72
         integer, parameter :: N_M = 49
 
         contains
 
-                function read_Fresnel( folder, file, N_F ) result(A)
+                function read_Fresnel( folder, file, N_F ) result(NF)
                         character(len=*), intent(in) :: folder 
                         character(len=*), intent(in) :: file 
-                        real :: A(N_F,N_R,N_E)
+                        integer, intent(in) :: N_F
+                        complex :: NF(N_F,N_R,N_E)
+                        
+                        integer :: f_unit, io_stat, n_records, i
+                        character(len=:), allocatable :: file_route
+                        real :: data(N_M*N_F*N_E, 4), tmp
+                        complex :: A(N_F, N_M, N_E), U(N_F, N_M, N_E)
 
-                        A = 0
+                        file_route = folder // file 
+                        print*, "reading ", file_route, " ..."
+                        open( file=file_route, status="old", action="read", newunit=f_unit, iostat=io_stat )
+                        if (io_stat == 0) print*, "in unit ", f_unit
+
+                        do i = 1, 10
+                                read(unit=f_unit,fmt=*)
+                        end do
+
+                        n_records = N_F*N_M*N_E
+                        do i = 1, n_records
+                                read(unit=f_unit, fmt=*) tmp, tmp, tmp, data(i,1), data(i,2), data(i,3), data(i,4)
+                        end do
+
+                        U = cmplx( reshape( data(:,1), shape=[N_F,N_M,N_E]), -reshape( data(:,2), shape=[N_F,N_M,N_E]) )
+                        A = cmplx( reshape( data(:,3), shape=[N_F,N_M,N_E]), -reshape( data(:,4), shape=[N_F,N_M,N_E]) )
+
+                        
+
+
+
+
+
+
                 end function
 
 
--- test_FresnelLoaders.f90
+++ test_FresnelLoaders.f90
@@ -1,8 +1,12 @@
 program test_fresnel
 
-        use FresnelLoaders, only: read_Fresnel
+        use FresnelLoaders, only: read_Fresnel, N_R, N_E
+
+        integer, parameter :: N_F = 8
         
-        print*, read_Fresnel("","",4)
+        real :: A(N_F,N_R,N_E)
+
+        A = read_Fresnel("./Data/", "uTM_shaped.txt", N_F)
 
 
 end program test_fresnel
